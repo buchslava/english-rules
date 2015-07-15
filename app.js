a778696ba91c3f245a1f52be091b1a5d64bdbb6d
buchslava <Zachet911>
2015-07-15 23:11:14 +0300

    not completed

--- app.js
+++ app.js
@@ -3,30 +3,30 @@ angular.module('eng.main', []);
 angular.module('eng.main')
 .controller('GeneralCtrl', [
   function() {
-    var subjectPronounsData = {
-      first: {
-        singular: values.i,
-        plural: values.we
-      },
-      second: {
-        singular: values.you,
-        plural: values.you
-      },
-      third: {
-        singular: {
-          male: values.he,
-          female: values.she,
-          inanimate: values.it
-        },
-        plural: {
-          values.it
-        }
-      }
-    };
 
-    this.rules = [
-      {
-        name: 'Subject pronouns',
+    var model = {
+      subjectPronouns: {
+        data: {
+          first: {
+            singular: this.values.i, plural: this.values.we
+          },
+          second: {
+            singular: values.you, plural: values.you
+          },
+          third: {
+            singular: {
+              male: this.values.he,
+              female: this.values.she,
+              inanimate: this.values.it
+            },
+            plural: values.it
+          }
+        },
+        params: [
+          {name: 'sort', cases: ['first', 'second', 'third']},
+          {name: 'tally', cases: ['singular', 'plural']},
+          {name: 'kind', cases: ['male', 'female', 'inanimate']}
+        ],
         values: {
           i: {name: 'I'},
           you: {name: 'you'},
@@ -35,14 +35,17 @@ angular.module('eng.main')
           it: {name: 'it'},
           we: {name: 'we'},
           they: {name: 'they'}
-        },
-        one: function (params) {
+        }
+      }
+    };
+
+    function one(modelKey, descriptor) {
+      for (var i = 0; i < model[modelKey].params.length; i++) {
+        if (typeof model[modelKey].params[i] === 'object') {
 
         }
       }
-    ];
+    }
 
-
-
-    this.foo = this.rules[0].one(['singular', 'male', 'third']);
+    this.foo = one('subjectPronouns', {tally: 'singular', kind: 'male', sort: 'third'});
   }]);
