44409e952ba0f025f07451079b21d8b1a17b5b1f
buchslava <vyacheslav.chub@gmail.com>
2015-07-15 18:53:15 +0300

    imprevement

--- app.js
+++ app.js
@@ -1,3 +1,48 @@
-function TodoCtrl($scope) {
-  $scope.foo = 'bar';
-}
+angular.module('eng.main', []);
+
+angular.module('eng.main')
+.controller('GeneralCtrl', [
+  function() {
+    var subjectPronounsData = {
+      first: {
+        singular: values.i,
+        plural: values.we
+      },
+      second: {
+        singular: values.you,
+        plural: values.you
+      },
+      third: {
+        singular: {
+          male: values.he,
+          female: values.she,
+          inanimate: values.it
+        },
+        plural: {
+          values.it
+        }
+      }
+    };
+
+    this.rules = [
+      {
+        name: 'Subject pronouns',
+        values: {
+          i: {name: 'I'},
+          you: {name: 'you'},
+          he: {name: 'he'},
+          she: {name: 'she'},
+          it: {name: 'it'},
+          we: {name: 'we'},
+          they: {name: 'they'}
+        },
+        one: function (params) {
+
+        }
+      }
+    ];
+
+
+
+    this.foo = this.rules[0].one(['singular', 'male', 'third']);
+  }]);
