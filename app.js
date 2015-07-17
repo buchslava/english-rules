5181f6e7a92611cb4a1ac028d04869d0e8c7f4a1
buchslava <vyacheslav.chub@gmail.com>
2015-07-17 17:48:53 +0300

    dynamic rules

--- app.js
+++ app.js
@@ -1,51 +1,132 @@
-angular.module('eng.main', []);
+angular.module('eng.main', ['ui.bootstrap']);
 
 angular.module('eng.main')
 .controller('GeneralCtrl', [
   function() {
 
-    var model = {
-      subjectPronouns: {
-        data: {
-          first: {
-            singular: this.values.i, plural: this.values.we
-          },
-          second: {
-            singular: values.you, plural: values.you
-          },
-          third: {
-            singular: {
-              male: this.values.he,
-              female: this.values.she,
-              inanimate: this.values.it
-            },
-            plural: values.it
-          }
+    var self = this;
+
+    this.arrSliceToStr = function arrSliceToStr(arr, name, div) {
+      return _.pluck(arr, name).join(div || ' ');
+    };
+
+    this.terms = {
+      partsOfSpeech: {
+        noun: {
+          ru: 'существительное',
+          desc: 'кто/что'
         },
-        params: [
-          {name: 'sort', cases: ['first', 'second', 'third']},
-          {name: 'tally', cases: ['singular', 'plural']},
-          {name: 'kind', cases: ['male', 'female', 'inanimate']}
-        ],
-        values: {
-          i: {name: 'I'},
-          you: {name: 'you'},
-          he: {name: 'he'},
-          she: {name: 'she'},
-          it: {name: 'it'},
-          we: {name: 'we'},
-          they: {name: 'they'}
+        pronoun: {
+          ru: 'местоимение',
+          desc: 'кто/что'
+        },
+        verb: {
+          ru: 'глагол',
+          desc: 'что делать'
+        },
+        adverb: {
+          ru: 'наречие',
+          desc: 'как где куда зачем почему'
+        },
+        article: {
+          ru: 'артикль',
+          desc: 'a the'
+        },
+        preposition: {
+          ru: 'предлог',
+          desc: 'on to in'
+        },
+        numerals: {
+          ru: 'числительные',
+          desc: 'порядковые количественные'
         }
       }
     };
 
-    function one(modelKey, descriptor) {
-      for (var i = 0; i < model[modelKey].params.length; i++) {
-        if (typeof model[modelKey].params[i] === 'object') {
+    this.values = {
+      i: {name: 'I'},
+      you: {name: 'you'},
+      he: {name: 'he'},
+      she: {name: 'she'},
+      it: {name: 'it'},
+      we: {name: 'we'},
+      they: {name: 'they'},
+      am: {name: 'am'},
+      is: {name: 'is'},
+      are: {name: 'are'}
+    };
 
+    this.rule = {
+      subjectPronouns: {
+        first: function () {
+          return {
+            singular: function () {
+              return self.values.i;
+            },
+            plural: function () {
+              return self.values.we;
+            }
+          };
+        },
+        second: function () {
+          return self.values.you;
+        },
+        third: function () {
+          return {
+            singular: function () {
+              return {
+                male: function () {
+                  return self.values.he;
+                },
+                female: function () {
+                  return self.values.she
+                },
+                inanimate: function () {
+                  return self.values.it;
+                }
+              };
+            },
+            plural: function () {
+              return self.values.it;
+            }
+          };
+        }
+      },
+      tobe: {
+        i: function () {
+          return self.values.am;
+        },
+        you: function () {
+          return self.values.are;
+        },
+        he: function () {
+          return self.values.is;
+        },
+        she: function () {
+          return self.values.is;
+        },
+        it: function () {
+          return self.values.is;
+        },
+        we: function () {
+          return self.values.are;
+        },
+        they: function () {
+          return self.values.are;
+        },
+        am: function () {
+          return [self.values.i];
+        },
+        is: function () {
+          return [self.values.he, self.values.she, self.values.it];
+        },
+        are: function () {
+          return [self.values.you, self.values.we, self.values.they];
         }
       }
-    }
+    };
 
-    this.foo = one('subjectPronouns', {tally: 'singular', kind: 'male', sort: 'third'});
+    this.toBeEx = {
+      positive: 'I'
+    }
   }]);
