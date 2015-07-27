3b108898fcf944b7150a968af99f38157b1df45b
buchslava <Zachet911>
2015-07-27 22:29:13 +0300

    remove unexpected

--- app.js
+++ app.js
@@ -1,132 +1,24 @@
 angular.module('eng.main', ['ui.bootstrap']);
 
 angular.module('eng.main')
-.controller('GeneralCtrl', [
-  function() {
-
-    var self = this;
-
-    this.arrSliceToStr = function arrSliceToStr(arr, name, div) {
-      return _.pluck(arr, name).join(div || ' ');
-    };
-
-    this.terms = {
-      partsOfSpeech: {
-        noun: {
-          ru: 'существительное',
-          desc: 'кто/что'
-        },
-        pronoun: {
-          ru: 'местоимение',
-          desc: 'кто/что'
-        },
-        verb: {
-          ru: 'глагол',
-          desc: 'что делать'
-        },
-        adverb: {
-          ru: 'наречие',
-          desc: 'как где куда зачем почему'
-        },
-        article: {
-          ru: 'артикль',
-          desc: 'a the'
-        },
-        preposition: {
-          ru: 'предлог',
-          desc: 'on to in'
-        },
-        numerals: {
-          ru: 'числительные',
-          desc: 'порядковые количественные'
+  .directive("quizWithOptions", ['$compile', function ($compile) {
+    return {
+      restrict: 'AE',
+      link: function (scope, element) {
+        scope.foo = 'bar';
+        scope.fff = function () {
+          console.log('!!!');
+        };
+        //console.log(element[0].children);
+        for (var i = 0; i < element[0].children.length; i++) {
+          console.log(element[0].children[i].innerHTML, element[0].children[i].attributes);
         }
+        element.html($compile("<a href ng-click='fff()'>{{foo}}</a>")(scope));
       }
     };
+  }])
+  .controller('GeneralCtrl', [
+    function () {
 
-    this.values = {
-      i: {name: 'I'},
-      you: {name: 'you'},
-      he: {name: 'he'},
-      she: {name: 'she'},
-      it: {name: 'it'},
-      we: {name: 'we'},
-      they: {name: 'they'},
-      am: {name: 'am'},
-      is: {name: 'is'},
-      are: {name: 'are'}
-    };
-
-    this.rule = {
-      subjectPronouns: {
-        first: function () {
-          return {
-            singular: function () {
-              return self.values.i;
-            },
-            plural: function () {
-              return self.values.we;
-            }
-          };
-        },
-        second: function () {
-          return self.values.you;
-        },
-        third: function () {
-          return {
-            singular: function () {
-              return {
-                male: function () {
-                  return self.values.he;
-                },
-                female: function () {
-                  return self.values.she
-                },
-                inanimate: function () {
-                  return self.values.it;
-                }
-              };
-            },
-            plural: function () {
-              return self.values.it;
-            }
-          };
-        }
-      },
-      tobe: {
-        i: function () {
-          return self.values.am;
-        },
-        you: function () {
-          return self.values.are;
-        },
-        he: function () {
-          return self.values.is;
-        },
-        she: function () {
-          return self.values.is;
-        },
-        it: function () {
-          return self.values.is;
-        },
-        we: function () {
-          return self.values.are;
-        },
-        they: function () {
-          return self.values.are;
-        },
-        am: function () {
-          return [self.values.i];
-        },
-        is: function () {
-          return [self.values.he, self.values.she, self.values.it];
-        },
-        are: function () {
-          return [self.values.you, self.values.we, self.values.they];
-        }
-      }
-    };
-
-    this.toBeEx = {
-      positive: 'I'
-    }
-  }]);
+      var self = this;s
+    }]);
